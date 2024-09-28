676ea84306fde98f2f0567044ba07117fc4b6853
Shantanu <shantanuwable2003@gmail.com>
2024-09-28 23:33:32 +0530

    Refactor get_prompt function to include operating system parameter

--- app/gemini.py
+++ app/gemini.py
@@ -24,28 +24,40 @@ Let's say we have the following document that corresponds to a heading
 },
 """
 
-def get_prompt(heading_document: dict) -> str:
+def get_prompt(heading_document: dict, os: str = "", ) -> str:
     prompt =f"""
-    Objective: Generate a comprehensive BASH shell script which can be used for auditing, remediating and verifying the compliance standard for the following heading from a CIS/DISA Document:
+    Objective: Generate a comprehensive shell script (bash in Unix based systems or Powershell/batch scripts in Windows) OR a set of steps to be followed on the GUI (in some cases in Windows Operating system) which can be used for auditing, remediating and verifying the compliance standard for the following specification from a CIS/DISA Document:
     
-    Title: {heading_document["Title"]}
-    Profile Applicability: {heading_document["Profile Applicability"]}
+    Operating System on which the shell script needs to be executed: {os}
     
-    Description: {heading_document["Description"]}
+    If the operating system mentioned is Windows Operating System, please provide the steps to be followed in the GUI OR a Powershell/batch script (if that is possible for the given specification).
+    if the operating system mentioned is a Unix based system (e.g. Linux, Ubuntu, Fedora, OpenSUSE, etc.), please provide a bash script or a set of commands that can be executed in the terminal (if that is possible for the given specification).
     
-    Rationale: {heading_document["Rationale"]}
+    Title: {heading_document.get("Title", "")}
     
-    Impact: {heading_document["Impact"]}
+    Profile Applicability: {heading_document.get("Profile Applicability", "")}
     
-    Audit: {heading_document["Audit"]}
+    Description: {heading_document.get("Description", "")}
     
-    Remediation: {heading_document["Remediation"]}
+    Rationale: {heading_document.get("Rationale", "")}
     
-    Additional Information: {heading_document["Additional Information"]}
+    Impact: {heading_document.get("Impact", "")}
+    
+    Audit: {heading_document.get("Audit", "")}
+    
+    Remediation: {heading_document.get("Remediation", "")}
+    
+    Additional Information: {heading_document.get("Additional Information", "")}
+    
+    In some cases of the operating system being Windows, the audit or remediation steps may involve GUI based steps. In such cases, please provide the steps that need to be followed in the GUI.
     
     DO NOT HALLUCINATE.
+    Do not provide PowerShell script if the operating system mentioned above is Unix based and vice versa.
+        
+    When you are returning a Shell script, please ensure that the response is in Markdown format and the code block is in the correct language format.
+    When you are returning a set of steps to be followed in the GUI, please ensure that the response is in Markdown format.
     
-    Only give the BASH shell script without any extra text.
+    Do not give any extra text.
     """
     
     return prompt
